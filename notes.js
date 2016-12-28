8e2785819212cf4866e3bbf07f3afac7cc2e9140
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 17:02:29 -0500

    Update more code and notes

--- notes.js
+++ notes.js
@@ -14,3 +14,37 @@ Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], 'Builder1',
 Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
   'HarvesterBig2',
   {role: 'harvester'});
+
+
+// We have underscore/lodash?
+var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+// remove dead creep memory (GOOD IDEA)
+for(var name in Memory.creeps) {
+    if(!Game.creeps[name]) {
+        delete Memory.creeps[name];
+        console.log('Clearing non-existing creep memory:', name);
+    }
+}
+// activates safe mode
+Game.spawns['Spawn1'].room.controller.activateSafeMods();
+
+// builds tower at specified location
+Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
+
+// Control by object ID
+var tower = Game.getObjectById('b9a97ddb5ea72b9ffde714bc');
+if(tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => structure.hits < structure.hitsMax
+    });
+    if(closestDamagedStructure) {
+        tower.repair(closestDamagedStructure);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if(closestHostile) {
+        tower.attack(closestHostile);
+    }
+}
--- role.harvester.js
+++ role.harvester.js
@@ -11,7 +11,9 @@ module.exports = {
     else {
       var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
-          return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+          return (structure.structureType == STRUCTURE_EXTENSION ||
+            structure.structureType == STRUCTURE_SPAWN ||
+            structure.structureType == STRUCTURE_TOWER) &&
             structure.energy < structure.energyCapacity;
         }
       });
