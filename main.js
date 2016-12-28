3cb21d5809b59a1e988411212ece68f40424056d
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 16:34:18 -0500

    Update syntax and whitespace

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ const roleBuilder = require('role.builder');
 
 
 module.exports.loop = function () {
-    for(let name in Game.rooms) {
-        console.log(`Room ${name} has ${Game.rooms[name].energyAvailable} energy`);
-    }
+  for (let name in Game.rooms) {
+    console.log(`Room ${name} has ${Game.rooms[name].energyAvailable} energy`);
+  }
 
-    for(let name in Game.creeps) {
-        let creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
+  for (let name in Game.creeps) {
+    let creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester') {
+      roleHarvester.run(creep);
     }
+    if (creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
+    }
+  }
 }
--- notes.js
+++ notes.js
@@ -1,61 +1,16 @@
-module.exports.loop = function () {
-    var creep = Game.creeps['Harvester1'];
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
-    }
-}
+// spawn creep
+Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], 'Upgrader1');
 
 
-module.exports.loop = function () {
-    var creep = Game.creeps['Harvester1'];
-
-    if(creep.carry.energy < creep.carryCapacity) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[0]);
-        }
-    }
-    else {
-        if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-            creep.moveTo(Game.spawns['Spawn1']);
-        }
-    }
-}
-
-module.exports.loop = function () {
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
-            }
-        }
-    }
-}
-
-Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Upgrader1' );
-
+// set memory
 Game.creeps['Harvester1'].memory.role = 'harvester';
 Game.creeps['Upgrader1'].memory.role = 'upgrader';
 
 
+// spawn creep with memory
+Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], 'Builder1',
+  {role: 'builder'}); // memory
 
-
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-
-Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Builder1',
-    { role: 'builder' } ); // memory
-
-    Game.spawns['Spawn1'].createCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
-    'HarvesterBig2',
-    { role: 'harvester' } );
+Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
+  'HarvesterBig2',
+  {role: 'harvester'});
