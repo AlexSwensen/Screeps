68f4df7ccf5ea7a70f27a4ce14e921899a131da4
Alexander Swensen <alex.swensen@gmail.com>
2016-12-29 14:40:41 -0500

    more logging

--- room.controller.js
+++ room.controller.js
@@ -16,7 +16,7 @@ const roomController = {
   },
   runRoomCreeps: function (room) {
     for (let name in room.creeps) {
-      // console.log(name);
+      console.log(name);
       let creep = room.creeps[name];
       // console.log(JSON.stringify(creep));
       if (creep.memory.role == 'harvester') {
