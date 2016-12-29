bab16d6ba7a47362ac89341d1f8502613bf8ed03
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 22:09:58 -0500

    OOps

--- bot.factory.js
+++ bot.factory.js
@@ -8,9 +8,8 @@ module.exports = {
       harvesters: _(Game.creeps).filter({memory: {role: 'harvester'}}),
       builders: _(Game.creeps).filter({memory: {role: 'builders'}}),
       defenders: _(Game.creeps).filter({memory: {role: 'defenders'}}),
-      upgraders: _(Game.creeps).filter({memory: {role: 'upgraders'}}),
-
-      console.log(`you have ${defenders.count()} defenders`);
+      upgraders: _(Game.creeps).filter({memory: {role: 'upgraders'}})
     };
+    console.log(`you have ${defenders.count()} defenders`);
   }
 };
