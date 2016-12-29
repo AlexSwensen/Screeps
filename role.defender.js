569b424e96fe398d0e03a151db49f47fb0c37415
Alexander Swensen <alex.swensen@gmail.com>
2016-12-29 00:29:36 -0500

    adjust

--- role.defender.js
+++ role.defender.js
@@ -3,7 +3,14 @@ module.exports = {
     creep.memory.target = creep.room.find(FIND_HOSTILE_CREEPS);
     if (creep.memory.target[0]) {
       if (_.some(creep.memory.target[0].body, {type: RANGED_ATTACK})) {
-        // We need numbers to attack ranged.
+        // We need numbers to attack ranged. for now we will just move them out of the way
+        creep.memory.destination = creep.room.find(FIND_MY_STRUCTURES, {
+        filter: (structure) => {
+          return structure.structureType == STRUCTURE_CONTROLLER;
+        }
+      });
+
+      creep.moveTo(creep.memory.destination[0]);
       } else {
         creep.say('attacking!');
         creep.moveTo(creep.memory.target[0]);
