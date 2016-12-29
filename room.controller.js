3480e4bb2ac70973ce2790ca952721dea00eddee
Alexander Swensen <alex.swensen@gmail.com>
2016-12-29 14:22:45 -0500

    Update for bugs

--- room.controller.js
+++ room.controller.js
@@ -5,25 +5,26 @@ const roomController = {
     for (let room in Game.rooms) {
       RoomObject.prototype.creeps = RoomObject.find(FIND_MY_CREEPS);
       rooms.push(room);
+      console.log(JSON.stringify(room));
       this.runRoomCreeps(room);
     }
   },
   runRoomCreeps: function (room) {
     for (let creep in room.creeps) {
-    // let creep = Game.creeps[name];
-    if (creep.memory.role == 'harvester') {
-      roleHarvester.run(creep);
+      // let creep = Game.creeps[name];
+      if (creep.memory.role == 'harvester') {
+        roleHarvester.run(creep);
+      }
+      if (creep.memory.role == 'upgrader') {
+        roleUpgrader.run(creep);
+      }
+      if (creep.memory.role == 'builder') {
+        roleBuilder.run(creep);
+      }
+      if (creep.memory.role == 'defender') {
+        roleDefender.run(creep);
+      }
     }
-    if (creep.memory.role == 'upgrader') {
-      roleUpgrader.run(creep);
-    }
-    if (creep.memory.role == 'builder') {
-      roleBuilder.run(creep);
-    }
-    if (creep.memory.role == 'defender') {
-      roleDefender.run(creep);
-    }
-  }
   }
 };
 
