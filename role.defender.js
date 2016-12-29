5b42c624f6807901e748686a971a5c1120016653
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 20:57:35 -0500

    Fix error

--- role.defender.js
+++ role.defender.js
@@ -1,6 +1,6 @@
 module.exports = {
   run: creep => {
-    var target = creep.room.findClosestByRange(FIND_HOSTILE_CREEPS);
+    var target = creep.room.find(FIND_HOSTILE_CREEPS);
     if (creep.attack(target) == ERR_NOT_IN_RANGE) {
       creep.moveTo(target);
     }
