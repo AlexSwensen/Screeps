fe91d927629aaab2a484a1f568fb566f573440f1
Alexander Swensen <alex.swensen@gmail.com>
2016-12-29 16:28:22 -0500

    Harvesters say things

--- role.harvester.js
+++ role.harvester.js
@@ -5,6 +5,7 @@ module.exports = {
     if (creep.carry.energy < creep.carryCapacity) {
       var sources = creep.room.find(FIND_SOURCES);
       if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.say('harvesting');
         creep.moveTo(sources[0]);
       }
     }
@@ -19,11 +20,13 @@ module.exports = {
       });
       if (targets.length > 0) {
         if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.say('transferring');
           creep.moveTo(targets[0]);
         }
       }
       if(targets.length == 0 && creep.carry.energy == creep.carryCapacity) {
         creep.moveTo(23, 23);
+        creep.say(ladeda);
       }
     }
   }
