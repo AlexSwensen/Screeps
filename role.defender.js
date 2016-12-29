1ea0af436f98e183d6f1fe2bed1e2659b3e59c79
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 21:02:41 -0500

    Update

--- role.defender.js
+++ role.defender.js
@@ -1,8 +1,13 @@
 module.exports = {
   run: creep => {
     var target = creep.room.find(FIND_HOSTILE_CREEPS);
+    if (target) {
+      creep.say('attacking!');
       creep.moveTo(target);
       creep.attack();
+    } else {
+      say('no target');
+    }
 
   }
 };
