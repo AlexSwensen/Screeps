00668203783d93a7b64374ee8d5a12c855c5e2ad
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 22:13:23 -0500

    Helps to have the role names correct

--- bot.factory.js
+++ bot.factory.js
@@ -5,10 +5,10 @@ module.exports = {
   run: function () {
     this.spawns = Game.spawns;
     this.creeps = {
-      harvesters: _(Game.creeps).filter({memory: {role: 'harvester'}}),
-      builders: _(Game.creeps).filter({memory: {role: 'builders'}}),
-      defenders: _(Game.creeps).filter({memory: {role: 'defenders'}}),
-      upgraders: _(Game.creeps).filter({memory: {role: 'upgraders'}})
+      harvesters: _(Game.creeps).filter({memory: {role: 'harveste'}}),
+      builders: _(Game.creeps).filter({memory: {role: 'builder'}}),
+      defenders: _(Game.creeps).filter({memory: {role: 'defender'}}),
+      upgraders: _(Game.creeps).filter({memory: {role: 'upgrader'}})
     };
     console.log(`you have ${this.creeps.defenders.size()} defenders`);
   }
