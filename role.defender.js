3523f044d9d5a8a47d74d3d0dfd264361c63cf89
Alexander Swensen <alex.swensen@gmail.com>
2016-12-29 00:19:00 -0500

    For now, we don't want to deal with ranged attacks unless we have numbers.

--- role.defender.js
+++ role.defender.js
@@ -2,9 +2,13 @@ module.exports = {
   run: creep => {
     creep.memory.target = creep.room.find(FIND_HOSTILE_CREEPS);
     if (creep.memory.target[0]) {
-      creep.say('attacking!');
-      creep.moveTo(creep.memory.target[0]);
-      creep.attack();
+      if (creep.memory.target[0].body.includes(RANGED_ATTACK)) {
+
+      } else {
+        creep.say('attacking!');
+        creep.moveTo(creep.memory.target[0]);
+        creep.attack();
+      }
     } else {
       creep.memory.destination = creep.room.find(FIND_MY_STRUCTURES, {
         filter: (structure) => {
