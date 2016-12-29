d994efecc8fc12533cd0ad485bfd684ca22beab0
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 21:33:11 -0500

    Fix typo

--- bot.factory.js
+++ bot.factory.js
@@ -1,7 +1,7 @@
 module.exports = {
   spawns: [],
   updateSpawns: () => {
-    this.spawns = game.rooms[0].find(FIND_MY_STRUCTURES, {
+    this.spawns = Game.rooms[0].find(FIND_MY_STRUCTURES, {
       filter: structure => {
         return structure.structureType == STRUCTURE_SPAWN;
       }
