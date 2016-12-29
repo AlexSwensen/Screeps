1c0b7890994921ffe1a48c7ad805a66be806450e
Alexander Swensen <alex.swensen@gmail.com>
2016-12-28 21:01:57 -0500

    Will this work?

--- role.defender.js
+++ role.defender.js
@@ -1,8 +1,8 @@
 module.exports = {
   run: creep => {
     var target = creep.room.find(FIND_HOSTILE_CREEPS);
-    if (creep.attack(target) == ERR_NOT_IN_RANGE) {
       creep.moveTo(target);
-    }
+      creep.attack();
+
   }
 };
