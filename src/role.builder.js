3106a53971fd12f94e1f556aa4ea2e8cadd085ab
Alexander Swensen <alex.swensen@gmail.com>
2016-12-30 03:52:02 -0500

    Builders should go to closest energy source.

--- src/role.builder.js
+++ src/role.builder.js
@@ -22,9 +22,9 @@ module.exports = {
       }
     }
     else {
-      var sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+      var source = creep.pos.findClosestByRange(FIND_SOURCES); // range in order to increase efficiency in building roads.
+      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
       }
     }
   }
