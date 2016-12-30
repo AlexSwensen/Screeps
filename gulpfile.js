e1ec53ce63da591af3688dc523b79116bf18f121
Alexander Swensen <alex.swensen@gmail.com>
2016-12-29 23:58:00 -0500

    Add comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var screeps = require('gulp-screeps');
-var credentials = require('./credentials.js');
+var credentials = require('./credentials.js'); // see https://github.com/pcmulder/gulp-screeps
 
 gulp.task('default', ['screeps', 'watch']);
 gulp.task('watch', function() {
@@ -8,5 +8,4 @@ gulp.task('watch', function() {
 });
 gulp.task('screeps', function() {
   gulp.src('*.js').pipe(screeps(credentials));
-
 });
