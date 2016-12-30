5a5bf1d808a32f10a8b72e5ebddcba45b7d9262f
Alexander Swensen <alex.swensen@gmail.com>
2016-12-30 03:07:44 -0500

    upgraders will grab from storage units before harvesting

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -1,8 +1,9 @@
 /** role.upgrader */
-module.exports = {
+
+var roleUpgrader = {
 
   /** @param {Creep} creep **/
-  run: creep => {
+  run: function (creep) {
 
     if (creep.memory.upgrading && creep.carry.energy == 0) {
       creep.memory.upgrading = false;
@@ -19,10 +20,34 @@ module.exports = {
       }
     }
     else {
-      var sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+      var source = this.findEnergySource(creep);
+      if (source.structureType) {
+        this.harvestEnergyStorage(creep, source);
+      } else {
+        this.harvestEnergySource(creep, source)
       }
+    }
+  },
+  findEnergySource: function (creep) {
+    const source = creep.pos.findClosestByRange(FIND_SOURCES);
+    const container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: function (structure) {
+        return (structure.structureType == STRUCTURE_CONTAINER && _(structure.store).sum() > 0);
+      }
+    });
+
+    return container || source;
+  },
+  harvestEnergySource: function (creep, source) {
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(source);
+    }
+  },
+  harvestEnergyStorage: function (creep, structure) {
+    if (creep.withdraw(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(structure);
     }
   }
 };
+
+module.exports = roleUpgrader;
