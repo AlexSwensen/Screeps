d19acc4464bae9bc4dd30be149ed6c6ec3711272
Alexander Swensen <alex.swensen@gmail.com>
2016-12-29 21:55:19 -0500

    Refactor harvester code

--- role.harvester.js
+++ role.harvester.js
@@ -2,10 +2,18 @@
 var roleHarvester = {
 
   run: creep => {
-    if (creep.carry.energy < creep.carryCapacity) {
+    if (creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.harvesting = false;
+      creep.say('storing');
+    }
+    else if (!creep.memory.harvesting && creep.carry.energy < creep.carryCapacity) {
+      creep.memory.harvesting = true;
+      creep.say('harvesting');
+    }
+
+    if (creep.memory.harvesting) {
       var source = creep.pos.findClosestByRange(FIND_SOURCES);
       if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-        creep.say('harvesting');
         creep.moveTo(source);
       }
     }
@@ -21,11 +29,10 @@ var roleHarvester = {
       });
       if (targets.length > 0) {
         if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.say('transferring');
           creep.moveTo(targets[0]);
         }
       }
-      if(targets.length == 0 && creep.carry.energy == creep.carryCapacity) {
+      if (targets.length == 0 && creep.carry.energy == creep.carryCapacity) {
         creep.moveTo(23, 23);
         creep.say('ladeda');
       }
